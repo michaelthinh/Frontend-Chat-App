c0f2d0451d674ba8dfe4f7113ff00ee31d7a40ee
Michael Thinh <michaelthinh1008@gmail.com>
2024-09-21 10:26:00 +0700

    add: Create MainLayout

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
 import { Provider } from "react-redux";
 import store from "./store/store";
 
-createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
